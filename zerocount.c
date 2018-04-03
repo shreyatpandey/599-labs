03e6338b7aa4d42bb08973a58bac4a7a153a31a3
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2018-04-03 11:30:22 -0700

    Add files via upload

---
+++ zerocount.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int trailing_zero(long int input) /*Done with the help from geekforgeeks */
+{
+   int count_of_zeros;
+   count_of_zeros=0;
+	while(input>0)
+	{
+	  input=input/5;
+	  count_of_zeros = count_of_zeros + input;
+
+	}
+return(count_of_zeros);
+
+}
+
+
+
+int main(int argc,char *argv[])
+{
+	int answer;
+	if(argv[1]==NULL)
+	{
+	   printf("there is no input\n");
+	}
+	char *a = argv[1];
+  unsigned long int input = atoi(a);
+   if(input<0)
+   {
+        printf("No cannot be negative\n");
+   }
+   if(input<100)
+   {
+       printf("Test case is for no greater than 100\n");
+   }
+  answer=trailing_zero(input);
+  printf("No of trailing zeros are:%d\n",answer);
+
+}
